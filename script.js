0974d64cb3cc2a38dac40bad73cb0e0f8b483d09
GabrielRioo <gabrielpaivario@gmail.com>
2023-04-13 10:45:03 -0300

    Consumindo API de CEP e preenchendo o formulario de acordo com retorno do json

---
+++ script.js
@@ -0,0 +1,46 @@
+// var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/")
+//   .then((res) => res.json())
+//   .then((r) => {
+//     if (r.erro) {
+//       throw Error("Esse CEP não existe.");
+//     } else {
+//       console.log(r);
+//     }
+//   })
+//   .catch((error) => console.log(error))
+//   .finally(msg => console.log('Processamento concluído!'));
+
+// console.log(consultaCEP);
+
+async function buscaEndereco(cep) {
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPconvertida = await consultaCEP.json();
+
+    if (consultaCEPconvertida.erro) {
+        throw Error('CEP não existente!');
+    }
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+
+    cidade.value = consultaCEPconvertida.localidade;
+    logradouro.value = consultaCEPconvertida.logradouro;
+    estado.value = consultaCEPconvertida.uf;
+    
+    console.log(consultaCEPconvertida);
+
+    return consultaCEPconvertida
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
+
+
+
+// let ceps = ['01001000', '01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// Promise.all(conjuntoCeps).then(res => console.log(res));
